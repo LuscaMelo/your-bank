dea0bbd8c49b4299d59c946fb98949593f92c551
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-20 16:58:40 -0300

    [FEAT] Configuring the application theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-dark': '#1A1A1A',
+        'brand-primary': '#CAFF33',
+        'brand-secondary': '#1C1C1C',
+        'brand-grey': '#344D67',
+      },
+    },
   },
   plugins: [],
 }
