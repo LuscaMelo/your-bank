14275790ea12f34e9d1a9f2dfe266756f33b0256
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-20 16:25:56 -0300

    [FEAT] Configuring application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CareersComponent } from './pages/careers/careers.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SecurityComponent } from './pages/security/security.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'careers', component: CareersComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'security', component: SecurityComponent },
+    { path: 'signup', component: SignupComponent },
+];
