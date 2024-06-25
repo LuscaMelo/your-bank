e14c0f35c4dfe097442d12d386d528fab456ea3c
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-25 11:59:46 -0300

    [FEAT] Activating feature buttons with click

--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Button } from '../../models/button.model';
 
 @Component({
   selector: 'app-features',
@@ -10,5 +11,18 @@ import { Component, Input } from '@angular/core';
 })
 export class FeaturesComponent {
   @Input() props: any
+
+  //Features Buttons
+  featuresButtons: Button[] = [
+    { name: 'Online Banking', active: true },
+    { name: 'Financial Tools', active: false },
+    { name: 'Customer Support', active: false }
+  ]
+
+  changeFeatures(buttonClicked: string): void {
+    this.featuresButtons.map((btn: Button) => {
+      btn.name == buttonClicked ? btn.active = true : btn.active = false
+    })
+  }
 
 }
