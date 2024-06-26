e783aad331d3af3769526267289e071c8b691bd9
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-25 23:57:18 -0300

    [FIX] Adjusting card rendering in the products section

--- src/app/components/our-products/our-products.component.ts
+++ src/app/components/our-products/our-products.component.ts
@@ -14,6 +14,16 @@ export class OurProductsComponent {
 
   @Input() props: any
 
+  ngOnInit(): void {
+    this.individuals = this.productsCards.filter(card => card.category == 'individuals')
+    this.businesses = this.productsCards.filter(card => card.category == 'businesses')
+    this.productsCardList = this.individuals
+  }
+
+  individuals: any
+  businesses: any
+  productsCardList: any
+
   //Cards - Our Products
   productsCards: any[] = [
     { category: 'individuals', icon: 'products-icon1.png', title: 'Checking Accounts', text: 'Enjoy easy and convenient access to your funds with our range of checking account options. Benefit from features such as online and mobile banking, debit cards, and free ATM access.', border: false },
@@ -23,10 +33,6 @@ export class OurProductsComponent {
     { category: 'businesses', icon: 'products-icon5.png', title: 'Loans and Mortgages Plus', text: 'Realize your dreams with our flexible loan and mortgage options. From personal loans to home mortgages, our experienced loan officers are here to guide you through the application process and help you secure the funds you need.', border: true },
     { category: 'businesses', icon: 'products-icon4.png', title: 'Checking Accounts Plus', text: 'Enjoy easy and convenient access to your funds with our range of checking account options. Benefit from features such as online and mobile banking, debit cards, and free ATM access.', border: false },
   ]
-
-  individuals: any = this.productsCards.filter(card => card.category == 'individuals')
-  businesses: any = this.productsCards.filter(card => card.category == 'businesses')
-  productsCardList: any = this.individuals
 
   toggleCards(event: Event) {
     this.productsCardList == this.individuals ? this.productsCardList = this.businesses : this.productsCardList = this.individuals
