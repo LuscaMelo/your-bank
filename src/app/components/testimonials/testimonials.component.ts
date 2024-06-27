b31397f048ef729edf6942b8510a4972c59c5e25
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-27 01:38:54 -0300

    [FEAT] Working testimonials section slides

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -13,10 +13,23 @@ import { ToggleButtonComponent } from '../toggle-button/toggle-button.component'
 export class TestimonialsComponent {
   @Input() props: any
 
-  ngOnInit(): void {
-  }
-
+  //Testimonials data
+  testimonials: any = [
+    { name: 'Sara T', text: 'YourBank has been my trusted financial partner for years. Their personalized service and innovative digital banking solutions have made managing my finances a breeze.' },
+    { name: 'John D', text: "I recently started my own business, and YourBank has been instrumental in helping me set up my business accounts and secure the financing I needed. Their expert guidance and tailored solutions have been invaluable." },
+    { name: 'Emily G', text: "I love the convenience of YourBank's mobile banking app. It allows me to stay on top of my finances and make transactions on the go. The app is user-friendly and secure, giving me peace of mind." },
+  ]
 
   toggleCards(event: Event): void {
+  }
+
+  slideUp(): void {
+    let primeiro: any = this.testimonials.shift()
+    this.testimonials.push(primeiro)
+  }
+
+  slideDown(): void {
+    let ultimo: any = this.testimonials.pop()
+    this.testimonials.unshift(ultimo)
   }
 }
