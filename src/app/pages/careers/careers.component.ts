3da8bd48190b58a7cf5e6f04eeb5c70b739d4df6
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-28 00:59:03 -0300

    [FEAT] Creating overview component

--- src/app/pages/careers/careers.component.ts
+++ src/app/pages/careers/careers.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { OverviewComponent } from '../../components/overview/overview.component';
 
 @Component({
   selector: 'app-careers',
   standalone: true,
-  imports: [],
+  imports: [OverviewComponent],
   templateUrl: './careers.component.html',
   styleUrl: './careers.component.css'
 })
