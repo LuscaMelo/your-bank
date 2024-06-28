c82a5605d36fac8264be131808f6b76e1568e963
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-28 20:40:19 -0300

    [FEAT] Inserting FAQ component on the careers page

--- src/app/pages/careers/careers.component.ts
+++ src/app/pages/careers/careers.component.ts
@@ -3,14 +3,20 @@ import { OverviewComponent } from '../../components/overview/overview.component'
 import { OurValuesComponent } from '../../components/our-values/our-values.component';
 import { BenefitsComponent } from '../../components/benefits/benefits.component';
 import { JobsComponent } from '../../components/jobs/jobs.component';
+import { FaqComponent } from '../../components/faq/faq.component';
+import { CommonModule } from '@angular/common';
+import { SectionTitleComponent } from '../../components/section-title/section-title.component';
 
 @Component({
   selector: 'app-careers',
   standalone: true,
-  imports: [OverviewComponent, OurValuesComponent, BenefitsComponent, JobsComponent],
+  imports: [SectionTitleComponent, OverviewComponent, OurValuesComponent, BenefitsComponent, JobsComponent, FaqComponent],
   templateUrl: './careers.component.html',
   styleUrl: './careers.component.css'
 })
 export class CareersComponent {
+
+  //FAQ section title
+  faqTitle: any = { title: 'Frequently', emphasis: 'Asked Questions', subtitle: 'Still you have any questions? Contact our Team via support@yourbank.com' }
 
 }
