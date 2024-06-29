b5c18d58ad5165bad42b416acb38a967435f214a
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-29 17:47:36 -0300

    [REFACTOR] Making the overview component reusable and dynamic

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './overview.component.scss'
 })
 export class OverviewComponent {
-
+  @Input() overviewData: any
 }
--- src/app/pages/careers/careers.component.ts
+++ src/app/pages/careers/careers.component.ts
@@ -17,6 +17,14 @@ import { CallToAction } from '../../models/callToAction.model';
 })
 export class CareersComponent {
 
+  //Overview Data
+  overviewData: any = {
+    title: 'Welcome to ',
+    emphasys: 'YourBank Careers!',
+    description: 'Join our team and embark on a rewarding journey in the banking industry. At YourBank, we are committed to fostering a culture of excellence and providing opportunities for professional growth. With a focus on innovation, customer service, and integrity, we strive to make a positive impact in the lives of our customers and communities. Join us today and be a part of our mission to shape the future of banking.',
+    image: 'overview-careers.png'
+  }
+
   //Call To Action
   callToAction: CallToAction = {
     title: 'Start your Career with ',
