ff4d78143a883680dac722228d1590c7e27f90f2
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-07-01 09:39:50 -0300

    [FEAT] Inserting component overview on the security page

--- src/app/pages/security/security.component.ts
+++ src/app/pages/security/security.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Overview } from '../../models/overview.model';
+import { OverviewComponent } from '../../components/overview/overview.component';
 
 @Component({
   selector: 'app-security',
   standalone: true,
-  imports: [],
+  imports: [OverviewComponent],
   templateUrl: './security.component.html',
   styleUrl: './security.component.css'
 })
 export class SecurityComponent {
+
+  //Overview Data
+  overviewData: Overview = {
+    title: 'Your Security is Our ',
+    emphasys: 'Top Priority!',
+    description: 'At YourBank, we understand the importance of keeping your financial information secure. We employ robust security measures and advanced technologies to protect your personal and financial data. Rest assured that when you bank with us, your security is our utmost priority.',
+    image: 'overview-security.png'
+  }
 
 }
