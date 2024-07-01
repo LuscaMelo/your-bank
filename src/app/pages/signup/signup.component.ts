c26ed937a0919c8480e10f5cb6e55297073be9c9
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-07-01 16:26:03 -0300

    [FEAT] Adding sign up form and testimonials components to the sign up page

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
+import { SignupFormComponent } from '../../components/signup-form/signup-form.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [SignupFormComponent, TestimonialsComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
 
+
+  //Title
+  heading: any = {
+    title: 'Mission &', emphasis: ' Vision', subtitle: 'We envision being a leading force in the industry, driven by innovation, integrity, and inclusivity, creating a brighter financial future for individuals and businesses while maintaining a strong commitment to customer satisfaction and community development'
+  }
 }
