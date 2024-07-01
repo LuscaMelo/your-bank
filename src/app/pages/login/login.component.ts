ba5fe8d4a56f46546b87fd09c1f62f88fb6c8c00
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-07-01 15:53:00 -0300

    [FEAT] Inserting testimonials component on the login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, TestimonialsComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  //Title
+  heading: any = {
+    title: 'Mission &', emphasis: ' Vision', subtitle: 'We envision being a leading force in the industry, driven by innovation, integrity, and inclusivity, creating a brighter financial future for individuals and businesses while maintaining a strong commitment to customer satisfaction and community development'
+  }
 }
