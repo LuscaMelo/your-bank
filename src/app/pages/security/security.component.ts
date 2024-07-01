bc05c2b67474e2fa2b5115f5219df2ee4d43dff5
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-07-01 10:11:45 -0300

    [FEAT] Inserting FAQ section on security page

--- src/app/pages/security/security.component.ts
+++ src/app/pages/security/security.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Overview } from '../../models/overview.model';
 import { OverviewComponent } from '../../components/overview/overview.component';
 import { HowWeProtectYouComponent } from '../../components/how-we-protect-you/how-we-protect-you.component';
+import { SectionTitleComponent } from '../../components/section-title/section-title.component';
+import { FaqComponent } from '../../components/faq/faq.component';
 
 @Component({
   selector: 'app-security',
   standalone: true,
-  imports: [OverviewComponent, HowWeProtectYouComponent],
+  imports: [OverviewComponent, HowWeProtectYouComponent, SectionTitleComponent, FaqComponent],
   templateUrl: './security.component.html',
   styleUrl: './security.component.css'
 })
@@ -19,5 +21,8 @@ export class SecurityComponent {
     description: 'At YourBank, we understand the importance of keeping your financial information secure. We employ robust security measures and advanced technologies to protect your personal and financial data. Rest assured that when you bank with us, your security is our utmost priority.',
     image: 'overview-security.png'
   }
+
+  //FAQ section title
+  faqTitle: any = { title: 'Frequently', emphasis: 'Asked Questions', subtitle: 'Still you have any questions? Contact our Team via support@yourbank.com' }
 
 }
